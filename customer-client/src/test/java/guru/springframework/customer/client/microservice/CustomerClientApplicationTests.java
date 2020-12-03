6414ce3bbf9447b3dfdcfb738163ff2b730a7284
Bakhtier Rakhmatov <rakhmatov@gmail.com>
2020-12-02 22:51:25 -0500

    initial checking

---
+++ customer-client/src/test/java/guru/springframework/customer/client/microservice/CustomerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.customer.client.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
